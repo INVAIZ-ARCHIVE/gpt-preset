724649dc8aed8ba739b329936eb7c34320e38957
ckdwns9121 <ckdwns9121@naver.com>
2023-12-20 16:09:14 +0900

    # indesign 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 import { HostApplication } from 'src/gpt/dto/create-preset.dto';
 import { after_effects_messages } from './after-effects';
 import { hangule_message } from './hangeul';
+import { indesign_messages } from './indesign';
 import { illustrator_messages } from './illustrator';
 import { lightroom_messages } from './lightroom-classic';
 import { photoshop_messages } from './photoshop';
@@ -13,7 +14,7 @@ const messageMap = {
   [HostApplication.LightroomClassic]: lightroom_messages,
   [HostApplication.Photoshop]: photoshop_messages,
   [HostApplication.PremierePro]: premiere_messages,
-  [HostApplication.InDesign]: premiere_messages,
+  [HostApplication.InDesign]: indesign_messages,
 };
 
 export default messageMap;
